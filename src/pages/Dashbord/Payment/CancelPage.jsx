8e41f342debab93197e4a61bc988a87b36b5645f
Aftab Farhan <aftabfarhan324@gmail.com>
2025-11-21 03:04:24 +0600

    addes success page

---
+++ src/pages/Dashbord/Payment/CancelPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CancelPage = () => {
+    return (
+        <div>
+            CancelPage
+CancelPage
+        </div>
+    );
+};
+
+export default CancelPage;
---
+++ src/pages/Dashbord/Payment/SuccessPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SuccessPage = () => {
+    return (
+        <div>
+            SuccessPage
+SuccessPage
+        </div>
+    );
+};
+
+export default SuccessPage;
